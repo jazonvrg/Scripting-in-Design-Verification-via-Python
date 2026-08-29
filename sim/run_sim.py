726a28bc623c58e29a73032b0b69d653ec3c9159
jazonvrg <nghiavvv887@gmail.com>
2026-08-29 18:05:04 +0700

    Update

--- sim/run_sim.py
+++ sim/run_sim.py
@@ -37,16 +37,15 @@ if process.returncode == 0:
 		error_pattern = re.compile(r'(Error:|Fatal:|UVM_ERROR|UVM_FATAL)', re.IGNORECASE)
 		error_count = 0
 
-		with open("transcript", "r") as log_file:
-			for line in log_file:
-				if error_pattern.search(line):
-					error_count += 1
-
-		print("-" * 80)
-		if error_count > 0:
-			print(f"[FAILED] Test {args.testname} is failed! Capture {error_count} errors in transcript.")
-		else:
-			print(f"[PASSED] Test {args.testname} is perfectly passed.")
+	with open("transcript", "r") as log_file:
+		for line in log_file:
+			if error_pattern.search(line):
+				error_count += 1
+	print("-" * 80)
+	if error_count > 0:
+		print(f"[FAILED] Test {args.testname} is failed! Capture {error_count} errors in transcript.")
+	else:
+		print(f"[PASSED] Test {args.testname} is perfectly passed.")
 else:
 	print(f"[CRASH] Simulated process has failed or Makefile is error.")
 
